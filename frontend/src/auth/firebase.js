2d0625f47167124b483affc7130731e1cd8b501a
Jannabhatla Anish Harsha <anishharshaj@gmail.com>
2026-02-13 19:56:49 +0530

    Remove redundant firebaseConfig declaration

--- frontend/src/auth/firebase.js
+++ frontend/src/auth/firebase.js
@@ -1,7 +1,4 @@
 import { initializeApp } from "firebase/app";
 import { firebaseConfig } from "../../firebaseConfig";
- 
-const firebaseConfig = firebaseConfig;
-
 
 export const app = initializeApp(firebaseConfig);
